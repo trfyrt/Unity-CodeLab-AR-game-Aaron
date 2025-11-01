927b95c15dc5c31f67c6596bcae550d1f81a0826
Aaron Jevon Benedict Kongdoh <abenedict01@student.ciputra.ac.id>
2025-11-01 20:36:39 +0800

    Initial

---
+++ Assets/Starter Package/PackageSpawner.cs
@@ -0,0 +1,93 @@
+﻿/*
+ * Copyright 2021 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.XR.ARFoundation;
+
+public class PackageSpawner : MonoBehaviour
+{
+    public DrivingSurfaceManager DrivingSurfaceManager;
+    public PackageBehaviour Package;
+    public GameObject PackagePrefab;
+
+    public static Vector3 RandomInTriangle(Vector3 v1, Vector3 v2)
+    {
+        float u = Random.value;
+        float v = Random.value;
+
+        if (u + v > 1)
+        {
+            u = 1 - u;
+            v = 1 - v;
+        }
+
+        return u * v1 + v * v2;
+    }
+
+    public static Vector3 FindRandomLocation(ARPlane plane)
+    {
+        var mesh = plane.GetComponent<ARPlaneMeshVisualizer>().mesh;
+        var triangles = mesh.triangles;
+        var vertices = mesh.vertices;
+
+        // Pastikan ada cukup data
+        if (triangles.Length < 3 || vertices.Length < 3)
+            return plane.transform.position;
+
+        // Pilih segitiga acak
+        int triangleIndex = Random.Range(0, triangles.Length / 3);
+        int i1 = triangles[triangleIndex * 3];
+        int i2 = triangles[triangleIndex * 3 + 1];
+        int i3 = triangles[triangleIndex * 3 + 2];
+
+        // Dapatkan vertex segitiga
+        Vector3 v1 = vertices[i1];
+        Vector3 v2 = vertices[i2];
+        Vector3 v3 = vertices[i3];
+
+        // Titik acak di dalam segitiga
+        Vector3 randomInTriangle = RandomInTriangle(v2 - v1, v3 - v1) + v1;
+        Vector3 randomPoint = plane.transform.TransformPoint(randomInTriangle);
+
+        return randomPoint;
+    }
+
+
+    public void SpawnPackage(ARPlane plane)
+    {
+        var packageClone = GameObject.Instantiate(PackagePrefab);
+        packageClone.transform.position = FindRandomLocation(plane);
+
+        Package = packageClone.GetComponent<PackageBehaviour>();
+    }
+
+    private void Update()
+    {
+        var lockedPlane = DrivingSurfaceManager.LockedPlane;
+        if (lockedPlane != null)
+        {
+            if (Package == null)
+            {
+                SpawnPackage(lockedPlane);
+            }
+
+            var packagePosition = Package.gameObject.transform.position;
+            packagePosition.Set(packagePosition.x, lockedPlane.center.y, packagePosition.z);
+        }
+    }
+}
